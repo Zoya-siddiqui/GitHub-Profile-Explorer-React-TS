871fbdb45d3af4698a3cbbd39c18812912d48fc6
Zoya-siddiqui <zoyas3423@gmail.com>
2025-12-18 13:37:10 +0530

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
-// import Exercise1 from './Practice/Exercise1'
-// import Exercise2 from './Practice/Exercise2'
-// import Exercise3 from './Practice/Exercise3'
-// import Exercise4 from './Practice/Exercise4'
-// import Exercise5 from './Practice/Exercise5'
-// import Exercise6 from './Practice/Exercise6'
-// import Exercise7 from './Practice/Exercise7'
+
 import Project1 from './Practice/Project1'
 
 function App() {
@@ -17,15 +11,7 @@ function App() {
     <>
 <Project1/>
 
-  {/* <div className='container flex  flex-col w-full justify-center p-4 items-start min-h-screen max-w-80 mx-auto'>
-<Exercise1/>
-<Exercise2/>
-<Exercise3/>
-<Exercise4 title={'my card '} count={12}/>
-<Exercise5/>
-<Exercise6/>
-<Exercise7/>
-  </div> */}
+ 
     </>
   )
 }
